0b88a61b9e7a34ded378b56e9c8f1dd7fed6724d
anoukruhaak <anoukruhaak@gmail.com>
2015-08-24 02:38:03 +0200

    nothing works without a plugin file ;)

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+
+# name: TechTalks Video Loader
+# about: provide collections of points based on giving official answers
+# version: 0.1
+# authors: Benjamin Kampmann, Anouk Ruhaak
+# url: https://github.com/ligthyear
+
+PLUGIN_NAME = "discourse_video_loader".freeze
+
+load File.expand_path('../lib/video_loader.rb', __FILE__)
